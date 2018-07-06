dff8226b285f022554935c266c0e357c98199be8
liushoulong <240665110@qq.com>
2018-07-07 03:54:14 +0800

    122

--- src/main/java/cn/jxufe/web/controller/IndexController.java
+++ src/main/java/cn/jxufe/web/controller/IndexController.java
@@ -2,17 +2,30 @@ package cn.jxufe.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping
+@RequestMapping(value = "/base")
 public class IndexController {
 
-	@RequestMapping("/index")
+	@RequestMapping(value="/index" ,method = RequestMethod.GET)
 	public String list(HttpServletRequest request){
-		
+		Logger log = Logger.getLogger(IndexController.class);
+		log.info("---------index------------------");
 		return "index";
+	}
+
+
+	@RequestMapping(value="/login" ,method = RequestMethod.GET)
+	@ResponseBody
+	public String login(HttpServletRequest request){
+		Logger log = Logger.getLogger(IndexController.class);
+		log.info("------------login---------------");
+		return "nihao";
 	}
 	
 }
